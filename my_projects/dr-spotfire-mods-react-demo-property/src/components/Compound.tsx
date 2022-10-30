d3a070ab2647457546257acf18a595761d3c7866
Christopher Rundell <cdrundell@icloud.com>
2022-10-30 18:24:42 +0000

    minor tweak

--- my_projects/dr-spotfire-mods-react-demo-property/src/components/Compound.tsx
+++ my_projects/dr-spotfire-mods-react-demo-property/src/components/Compound.tsx
@@ -8,9 +8,9 @@ interface Compound {
 
 class Compound extends React.Component<any, any, any> {
   render () {
-    const { X, Y, Z } = this.props
+    const { X, Y, Z, hexCode } = this.props
     return (
-      <div style={{backgroundColor: "red", padding: "8px", margin: "16px", color: "peachpuff", borderRadius: "8px" }}>
+      <div style={{backgroundColor: hexCode, padding: "8px", margin: "16px", color: "peachpuff", borderRadius: "8px" }}>
         <p>{ X }</p>
         <p>{ Y }</p>
         <p>{ Z }</p>
