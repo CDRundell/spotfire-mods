9fa6099b5fe65ca61810fea0d6a710343df8a32a
Christopher Rundell <cdrundell@icloud.com>
2022-12-06 14:38:00 +0000

    removed some redudant code

--- my_projects/dr-spotfire-mods-react-demo-property/src/app.tsx
+++ my_projects/dr-spotfire-mods-react-demo-property/src/app.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { AnalysisProperty } from "spotfire-api";
 import { CompoundList } from './components/CompoundList'
-// import { SearchBar } from './components/SearchBar'
 import { Header } from './components/Header'
 import { Postbox } from './components/Postbox'
 
@@ -12,24 +11,9 @@ export interface AppProps {
     rows: any,
     dataView: any,
     displayName: string
-    // showProperties: (x: number, y: number) => void;
 }
 
 class App extends React.Component <AppProps> {
-
-  constructor (props:any) {
-    super(props)
-    this.state = {
-      searchQuery: "",
-      categorical: false
-    }
-  }
-
-  search = (query:string) => {
-    this.setState({
-      searchQuery: query
-    })
-  }
 
   updateDocumentProperties = () => {
     const payloadObject = {
@@ -43,10 +27,6 @@ class App extends React.Component <AppProps> {
     }).pop()
     payload?.set(JSON.stringify(payloadObject))
     }
-    // let payload = this.props.properties.filter(item => {
-    //   return item.name === 'payload'
-    // }).pop()
-    // payload?.set(JSON.stringify({name: "LogP", value: 10.80}))
   }
 
 
@@ -54,7 +34,6 @@ class App extends React.Component <AppProps> {
     return (
       <div style={{ textAlign: "center" }}>
         <Header />
-        {/* <SearchBar searchFunction={this.search} /> */}
         <CompoundList {...this.props} {...this.state} />
         <Postbox {...this.props} propertiesFunction={this.updateDocumentProperties}  />
       </div>
